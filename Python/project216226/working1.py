eefac32f6f12ea61ebb1f401933e0b698860a1ba
Ahmad <149232415+ahmadktk1@users.noreply.github.com>
2026-06-22 10:17:07 -0700

    Create working1.py

---
+++ Python/project216226/working1.py
@@ -0,0 +1,10 @@
+items = hostelites.items()
+print(items)
+# print(items[0]["UserDetials"])
+
+items = list(items)
+print(items)
+
+print(items[0][1])
+print(items[2][1]["UserDetails"][0])
+# how to we find the person Id using his
